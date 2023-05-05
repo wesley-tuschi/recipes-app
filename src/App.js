66894e7abac9e6fc2958fc46b5ba73e1921b6c10
Wesley Tuschi <levituschi@gmail.com>
2023-05-05 18:22:22 -0300

    Header Component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/recipes" component={ Recipes } />
