37dc2cf7d7e5d4e2d468ee4714d09352ca1e48a3
MarcosRanauro <marcosranauro@hotmail.com>
2023-05-05 16:40:40 -0300

    FAQ-Tela de Login
    Requisito 4 - Fiz a implementação das validações que o requisito pede

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Login() {
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const passwordRegex = /^[0-9a-zA-Z]{7,}$/;
+
+    if (emailRegex.test(email) && passwordRegex.test(password)) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [email, password]);
+
+  const handleChange = (target) => {
+    const { name, value } = target;
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
   return (
     <div>
       <h1>Login</h1>
@@ -10,16 +31,21 @@ export default function Login() {
           placeholder="Email"
           name="email"
           data-testid="email-input"
+          value={ email }
+          onChange={ ({ target }) => handleChange(target) }
         />
         <input
           type="password"
           placeholder="Password"
           name="password"
           data-testid="password-input"
+          value={ password }
+          onChange={ ({ target }) => handleChange(target) }
         />
         <button
           type="button"
           data-testid="login-submit-btn"
+          disabled={ isDisabled }
         >
           ENTER
         </button>
@@ -27,3 +53,5 @@ export default function Login() {
     </div>
   );
 }
+
+export default Login;
