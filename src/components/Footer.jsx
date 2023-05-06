cc511d9adaaeffdb40a1ba727a0866e438f1d21d
MarcosRanauro <marcosranauro@hotmail.com>
2023-05-06 00:10:12 -0300

    FAQ-Erro de lint
    Arrumei os erros de lint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
-import '../Footer.css';
+import './styles/Footer.css';
 
 function Footer() {
   const history = useHistory();
