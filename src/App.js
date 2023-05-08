b74cf954b2d29e4ec3dd1ffa9edd82039844547d
vieira <vini.clau0@gmail.com>
2023-05-08 14:29:26 -0300

    resolve conflito

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
+import Footer from './components/Footer';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/recipes" component={ Recipes } />
+        <Route exact path="/profile" component={ Profile } />
       </Switch>
+      <Footer />
     </div>
   );
 }
