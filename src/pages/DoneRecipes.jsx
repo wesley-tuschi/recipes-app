362dc1efc701decc94cb0f128e0ecd3fb6c0f7bf
vieira <vini.clau0@gmail.com>
2023-05-12 19:23:27 -0300

    feat: encaminha a tela de done recipe

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,62 +1,166 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
 
 function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filter, setFilter] = useState('All');
+  const [copy, setCopy] = useState('');
   const history = useHistory();
-  const [userEmail, setUserEmail] = useState('');
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user?.email) {
-      setUserEmail(user.email);
+    const doneRecipe = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (doneRecipe) {
+      setDoneRecipes(doneRecipe);
     }
   }, []);
 
-  const handleButtonClick = (path) => () => {
-    history.push(path);
-  };
+  const filteredRecipes = doneRecipes.filter((recipe) => {
+    if (filter === 'All') {
+      return true;
+    } if (filter === 'Meals') {
+      return recipe.type === 'meal';
+    }
+    return recipe.type === 'drink';
+  });
 
-  const handleClickLogout = () => {
-    localStorage.clear();
-    history.push('/');
+  const filterMeals = () => setFilter('Meals');
+  const filterDrinks = () => setFilter('Drinks');
+  const clearFilter = () => setFilter('All');
+
+  const copyToClipboard = (id) => {
+    navigator.clipboard.writeText(`http://localhost:3000/meals/${id}`);
+
+    setCopy('Link copied!');
   };
 
   return (
-    <header>
-      <button onClick={ () => history.push('/profile') }>
-        <img
-          src={ profileIcon }
-          alt="Profile Icon"
-          data-testid="profile-top-btn"
-        />
-      </button>
-      <h2 data-testid="page-title">Done Recipes</h2>
-      <div>
-        <p data-testid="profile-email">{ userEmail }</p>
-      </div>
+    <div>
+      <header>
+        <button onClick={ () => history.push('/profile') }>
+          <img
+            src={ profileIcon }
+            alt="Profile Icon"
+            data-testid="profile-top-btn"
+          />
+        </button>
+        <h2 data-testid="page-title">Done Recipes</h2>
+        <button
+          onClick={ clearFilter }
+          data-testid="filter-by-all-btn"
+        >
+          All
 
-      <button
-        data-testid="profile-done-btn"
-        onClick={ handleButtonClick('/done-recipes') }
-      >
-        Done Recipes
-      </button>
+        </button>
+        <button
+          onClick={ filterMeals }
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
 
-      <button
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
+        </button>
+        <button
+          onClick={ filterDrinks }
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
 
-      <button
-        data-testid="profile-logout-btn"
-        onClick={ handleClickLogout }
-      >
-        Logout
-      </button>
-    </header>
+        </button>
+
+      </header>
+      <section>
+        <ul>
+          {filteredRecipes?.map((recipe, index) => (recipe.type === 'drink' ? (
+            <li key={ recipe.name }>
+              <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                <img
+                  src={ recipe.image }
+                  alt="meal img"
+                  data-testid={ `${index}-horizontal-image` }
+                />
+              </Link>
+              <Link to={ `${recipe.type}s/${recipe.id}` }>
+                <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+              </Link>
+              <span
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {recipe.alcoholicOrNot}
+
+              </span>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {recipe.doneDate}
+
+              </p>
+              <button
+                type="button"
+                onClick={ () => copyToClipboard(recipe.id) }
+              >
+                <img
+                  src={ shareIcon }
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  alt="share button"
+                />
+              </button>
+              {copy && <p>{ copy }</p>}
+            </li>
+          )
+            : (
+              <li key={ recipe.name }>
+                <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                  <img
+                    src={ recipe.image }
+                    alt="meal img"
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+                {console.log(index)}
+                <Link to={ `${recipe.type}s/${recipe.id}` }>
+                  <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+                </Link>
+                <span
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {recipe.nationality}
+                  {' '}
+                  -
+                  {' '}
+                  {recipe.category}
+                </span>
+                <p
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  {recipe.doneDate}
+
+                </p>
+                {recipe.tags.map((tag) => (
+                  <p
+                    key={ tag }
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                  >
+                    {tag}
+
+                  </p>
+                ))}
+                <button
+                  type="button"
+                  onClick={ () => copyToClipboard(recipe.id) }
+                >
+                  <img
+                    src={ shareIcon }
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    alt="share button"
+                  />
+                </button>
+                {copy && <p>{ copy }</p>}
+              </li>
+            )))}
+        </ul>
+      </section>
+    </div>
   );
 }
 
